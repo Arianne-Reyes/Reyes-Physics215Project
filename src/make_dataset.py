7eac8f4f8dbce2dbbfde0e247fc565fe8a10e607
Arianne-Reyes <113005712+Arianne-Reyes@users.noreply.github.com>
2025-05-25 13:37:54 +0800

    Create make_dataset.py

---
+++ src/make_dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.datasets import load_diabetes
+import os
+
+data = load_diabetes()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['target'] = data.target
+
+os.makedirs("data/raw", exist_ok=True)
+df.to_csv("data/raw/diabetes_raw.csv", index=False)
