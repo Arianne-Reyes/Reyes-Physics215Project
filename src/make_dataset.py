d9b4e94eede2ee5dbd85280101e3a4bd95c0f7e1
Arianne-Reyes <113005712+Arianne-Reyes@users.noreply.github.com>
2025-05-25 14:24:02 +0800

    Update make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sklearn.datasets import load_diabetes
 import os
 
-data = load_diabetes()
-df = pd.DataFrame(data.data, columns=data.feature_names)
-df['target'] = data.target
+def save_raw_data():
+    data = load_diabetes()
+    df = pd.DataFrame(data.data, columns=data.feature_names)
+    df['target'] = data.target
 
-# Ensure the folder exists before saving
-os.makedirs("data/raw", exist_ok=True)
-df.to_csv("data/raw/diabetes_raw.csv", index=False)
+    os.makedirs("data/raw", exist_ok=True)
+    df.to_csv("data/raw/diabetes_raw.csv", index=False)
