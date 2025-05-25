dcebd022dea99c6cf2cd4cd316098485f07a34c4
Arianne-Reyes <113005712+Arianne-Reyes@users.noreply.github.com>
2025-05-25 14:08:58 +0800

    Update make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from sklearn.datasets import load_diabetes
-import os
 
 data = load_diabetes()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['target'] = data.target
 
-os.makedirs("data/raw", exist_ok=True)
+# Assuming 'data/raw' folder already exists, so no need to create it here
 df.to_csv("data/raw/diabetes_raw.csv", index=False)
